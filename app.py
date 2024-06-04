679924e0d4be2005b0f358ddd7acead468899f77
NikKnez <nknezevic07@gmail.com>
2024-06-04 13:10:47 +0200

    Modified app.py

--- app.py
+++ app.py
@@ -49,9 +49,7 @@ if __name__ == "__main__":
     # Set the database to use
     app.config["DATABASE"] = myclient["db"]
 
-
-
     # Run the Flask application
-    # app.run(host="127.0.0.1", port=5000, debug=True)
     if __name__ == "__main__":
-        app.run(host="127.0.0.1", port=5000, debug=True)
+        port = int(os.environ.get("PORT", 5000))  # Use the PORT environment variable if available
+        app.run(host="0.0.0.0", port=port, debug=True)
